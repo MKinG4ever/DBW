f5589790309e5a063598d199f304e752430aa8bf
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2024-06-07 00:15:10 +0330

    User Object v1.1

--- User.py
+++ User.py
@@ -1,4 +1,11 @@
 class User:
+    """
+    A class representing a user with various attributes.
+
+    Author: Elmira Pour
+    Timestamp: 1717701765.2003505
+    """
+
     def __init__(self, username=None, name=None, email=None, mobile=None, birthday=None, password=None):
         """
         Initialize a new User instance.
@@ -10,7 +17,6 @@ class User:
         :param birthday: The birthday of the user in 'YYYY-MM-DD' format.
         :param password: The password for the user.
         """
-
         # Basic properties
         self.username = username
         self.name = name
@@ -18,9 +24,17 @@ class User:
         self.mobile = mobile
         self.birthday = birthday
         self.password = password
-
         # Dictionary to hold any additional properties
         self.extended_properties = {}
+
+    @property
+    def version(self):
+        """
+        Get the version of the User class.
+
+        :return: A string representing the version.
+        """
+        return f"v1.1"  # The current version of the User class
 
     def to_dict(self):
         """
@@ -28,7 +42,7 @@ class User:
 
         :return: A dictionary representation of the User object.
         """
-
+        # The basic properties as a dict
         user_dict = {
             "username": self.username,
             "name": self.name,
@@ -37,23 +51,23 @@ class User:
             "birthday": self.birthday,
             "password": self.password
         }
-
         # Add extended properties to the dictionary
         user_dict.update(self.extended_properties)
+        # Return the complete dictionary
         return user_dict
 
     def update_properties(self, **kwargs):
         """
-        Update the User object properties with provided keyword arguments.
+        Update the User object's properties with provided keyword arguments.
 
         :param kwargs: Dictionary of properties to update.
         """
-
+        # Update existing properties
         for key, value in kwargs.items():
-            # Convert keys to lower-case for Matching
+            # Convert key to lower-case for matching
             key = key.lower()
-            if hasattr(self, key):
-                setattr(self, key, value)
+            if hasattr(self, key):  # Check if the property exists in the class
+                setattr(self, key, value)  # Update the property value
 
     def extend_properties(self, **kwargs):
         """
@@ -61,9 +75,9 @@ class User:
 
         :param kwargs: Dictionary of new properties to add.
         """
-
+        # Add new properties not defined in the class
         for key, value in kwargs.items():
-            # Convert keys to lower-case for Matching
+            # Convert key to lower-case for matching
             key = key.lower()
-            if not hasattr(self, key):
-                self.extended_properties[key] = value
+            if not hasattr(self, key):  # Check if the property does not exist in the class
+                self.extended_properties[key] = value  # Add the new property to extended_properties
